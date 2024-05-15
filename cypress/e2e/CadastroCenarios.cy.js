6bc418281ae94f924c919f43dc74f678bf769e33
Adan Araujo De Souza <adanaraujo.souza@gmail.com>
2024-05-15 15:47:26 -0300

    Tratativa cadastro spec para não criar email duplicado e atualização do userObject removendo dados sensíveis

--- cypress/e2e/CadastroCenarios.cy.js
+++ cypress/e2e/CadastroCenarios.cy.js
@@ -10,10 +10,8 @@ describe('Pré-requirements definitions', function () {
     email = faker.internet.email();
     fullName = faker.name.fullName();
     password = faker.internet.password();
-    cy.registerNewUser(fullName,email,password, 'true').then((responseDados)=>{
-        console.log(responseDados)
-    })
 })
+
 context('Login page with user not registered', function () {
  
     it('Must render correctly', function () {
@@ -22,6 +20,10 @@ context('Login page with user not registered', function () {
       cy.get('[data-testid="email"]').should('be.enabled').and('be.visible').and('have.attr','placeholder','Digite seu email');
       cy.get('[data-testid="password"]').should('be.enabled').and('be.visible').and('have.attr','placeholder','Digite sua senha'); 
       cy.get('[data-testid="nome"]').should('be.enabled').and('be.visible').and('have.attr','placeholder','Digite seu nome');
+      cy.log('Cenary 2 - Redirect button validation');
+
+    cy.get('div>div>div>form>small>a').click();
+    cy.url().should('be.equal','https://front.serverest.dev/login')
   
     });
     it('Must allow the user to register with and without admin role', function () {
